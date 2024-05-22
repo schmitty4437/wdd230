07b7a4f6d687f5f2063b561c6b511b6060d9206f
schmitty4437 <schmitty4437@gmail.com>
2024-05-22 08:38:39 -0600

    w05 updates

--- chamber/scripts/dark-mode.js
+++ chamber/scripts/dark-mode.js
@@ -1,6 +1,5 @@
 const modeButton = document.querySelector('.switch input');
 const body = document.querySelector('body');
-const slider = document.querySelector('.slider:before');
 const headings = document.querySelectorAll('h2, h3, h4, h5, h6');
 
 modeButton.addEventListener('change', () => {
@@ -10,15 +9,11 @@ modeButton.addEventListener('change', () => {
         headings.forEach(heading => {
             heading.style.color = '#fff';
         });
-        // Update the SVG icon by setting the background image or content
-        slider.style.content = 'url(../images/light-mode-button.svg)';
     } else {
         body.style.background = '#F2EBDC';
         body.style.color = '#000';
         headings.forEach(heading => {
             heading.style.color = '#254559';
         });
-        // Update the SVG icon by setting the background image or content
-        slider.style.content = 'url(../images/dark-mode-button.svg)';
     }
 });
