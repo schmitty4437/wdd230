1e7c79c9b58d13560cc6c41a39d3fa7f6661c4dd
schmitty4437 <schmitty4437@gmail.com>
2024-06-18 14:30:07 -0600

    w09 fetch api update

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -6,17 +6,21 @@ async function getProphetData() {
     const data = await response.json();
 
     // console.table(data.prophets);
-    displayProphets(data.porophets);
+    displayProphets(data.prophets);
 }
-getProphetData();
 
 const displayProphets = (prophets) => {
     prophets.forEach((prophet) => {
         let card = document.createElement("section");
         let fullName = document.createElement("h2");
+        let birthday = document.createElement("p");
+        let birthplace = document.createElement("p");
         let portrait = document.createElement("img");
 
-        fullName.textContent = `${prophet.name} ${lastname}`;
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+
+        birthday.textContent = `Date of Birth: ${prophet.birthdate}`;
+        birthplace.textContent = `Place of Birth: ${prophet.birthplace}`;
 
         portrait.setAttribute('src', prophet.imageurl);
         portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
@@ -25,8 +29,12 @@ const displayProphets = (prophets) => {
         portrait.setAttribute('height', '440');
 
         card.append(fullName);
+        card.append(birthday);
+        card.append(birthplace);
         card.append(portrait);
 
         cards.appendChild(card);
     });
 }
+
+getProphetData();
