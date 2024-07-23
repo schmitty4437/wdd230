9756cfb42f9e7b97bd3c937afeebe989a547d906
schmitty4437 <schmitty4437@gmail.com>
2024-07-23 12:14:33 -0600

    scoots updates

--- scoots/scripts/weather.js
+++ scoots/scripts/weather.js
@@ -1,11 +1,10 @@
 // Select HTML elements in the document
 const currentTemp = document.querySelector('#current-temp');
 const icon = document.querySelector('#weather-icon');
-const description = document.querySelector('figcaption');
-const separator = document.querySelector('#separator');
 const forecastContainer = document.querySelector('#forecast-container');
 const banner = document.getElementById('banner');
 const currentHumidity = document.querySelector('#current-humidity');
+const weatherDescription = document.querySelector('#weather-description');
 const url = 'https://api.openweathermap.org/data/2.5/forecast?lat=20.48398585956197&lon=-86.96910183483305&units=imperial&appid=fbb8882852ed7e26477c5482b5a6b907';
 
 async function apiFetch() {
@@ -29,22 +28,18 @@ function displayResults(data) {
     const tempMax = Math.trunc(currentWeather.main.temp_max);
     const temp = Math.trunc(currentWeather.main.temp);
     const humidity = currentWeather.main.humidity;
-    currentTemp.innerHTML = `${temp}&deg;F`;
-    currentHumidity.innerHTML = `Humidity: ${humidity}%`;
-    const iconsrc = `https://openweathermap.org/img/w/${currentWeather.weather[0].icon}.png`;
 
-    let desc = currentWeather.weather[0].description;
-    desc = desc.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
-
-    icon.setAttribute('src', iconsrc);
-    icon.setAttribute('alt', desc);
-    description.textContent = `${desc}`;
-
-    if (currentTemp.textContent && desc) {
-        separator.style.display = 'inline';
-    } else {
-        separator.style.display = 'none';
+    // Update temperature, humidity, and description
+    if (currentTemp && currentHumidity && weatherDescription) {
+        currentTemp.innerHTML = `${temp}&deg;F`;
+        currentHumidity.innerHTML = `Humidity: ${humidity}%`;
+        const description = currentWeather.weather[0].description;
+        weatherDescription.innerHTML = description.charAt(0).toUpperCase() + description.slice(1);
     }
+
+    const iconsrc = `https://openweathermap.org/img/w/${currentWeather.weather[0].icon}.png`;
+    icon.setAttribute('src', iconsrc);
+    icon.setAttribute('alt', weatherDescription.textContent);
 
     // Banner with high temperature
     banner.innerHTML = `
@@ -52,7 +47,7 @@ function displayResults(data) {
         <button id="closeBanner">&times;</button>
     `;
 
-    // Close button
+    // Close button functionality
     const closeButton = document.getElementById('closeBanner');
     closeButton.addEventListener('click', function() {
         banner.classList.add('hidden');
@@ -82,7 +77,7 @@ function displayForecast(forecast) {
         const tempForecast = Math.trunc(forecast.main.temp);
         const descForecast = forecast.weather[0].description;
         const iconsrc = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
-        
+
         const forecastHTML = `
             <h3>Tomorrow at 3 PM</h3>
             <figure>
